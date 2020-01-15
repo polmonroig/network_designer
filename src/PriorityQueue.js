80f18ce43a35ff31b1448dd80b5c1b260f860575
Pol Monroig <polmonroig@yahoo.es>
2020-01-15 18:04:13 +0100

    Add MinHeap

--- src/PriorityQueue.js
+++ src/PriorityQueue.js
@@ -6,4 +6,67 @@
 
 class MinHeap{
 
+    constructor(){
+        this.heap = [];
+        this.size = 0;
+    }
+
+    push(x){
+        this.size++;
+        if(this.size === 1){
+            this.heap.push(null);
+            this.heap.push(x);
+        }
+        else{
+            self.heap.push(x);
+            this.insert_element(this.size);
+        }
+    }
+
+    top(){
+        return this.heap[0];
+    }
+
+    remove_element(pos){
+        let element = this.heap[pos];
+        while(2 * pos <= this.size){
+            let child = 2 * pos;
+            if(child !== this.size && this.heap[child] < this.heap[child + 1]){
+                child++;
+            }
+            if(element < this.heap[child]){
+                this.heap[pos] = this.heap[child];
+            }
+            else{
+                break;
+            }
+            pos = child;
+        }
+        this.heap[pos] = element;
+    }
+
+    insert_element(pos){
+        let newPos = Math.floor(pos / 2);
+        if(newPos > 0 && this.heap[newPos] < this.heap[pos]){
+            // swap values
+            let aux = this.heap[newPos];
+            this.heap[newPos] = this.heap[pos];
+            this.heap[pos] = aux;
+            this.insert_element(newPos);
+        }
+    }
+
+    pop(x){
+        if(this.empty())throw  "Heap is empty";
+        this.heap[1] = this.heap[this.size];
+        this.size--;
+        this.remove_element(1);
+        this.heap.splice(this.size - 1, 1);
+
+    }
+
+    empty(){
+        return this.size === 0;
+    }
+
 }
